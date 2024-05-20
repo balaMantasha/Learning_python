89d230989a5ad53118287a63fca3f5537848c74a
balaMantasha <balamantasha@gmail.com>
2024-05-20 22:33:29 +0530

    going with python

---
+++ Mutuationprob.py
@@ -0,0 +1,25 @@
+# def mutate_string(string, position, character):
+#     l=list(string)
+#     l[position] = character
+#     str1 = "".join(l)
+#     return str1
+# if __name__ == '__main__':
+#     s = input()
+#     i, c = input().split()
+#     s_new = mutate_string(s, int(i), c)
+#     print(s_new)
+# def count_substring(string, sub_string):
+#     return string.count(sub_string)
+
+# if __name__ == '__main__':
+#     string = input()
+#     sub_string = input()
+#     count = count_substring(string, sub_string)
+#     print(count)
+# import textwrap
+
+# if __name__ == '__main__':
+#     string, max_width = input(), int(input())
+#     result = textwrap.fill(string, max_width)
+#     print(result)
+
